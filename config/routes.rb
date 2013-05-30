159e3f1f8c0d053871f4ebdf2f03408ce3681144
Carsten Bokemeyer <carsten.bokemeyer@gmx.de>
2013-05-30 23:40:26 +0200

    first user tests

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ NerdCounter::Application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+   resources :users
+
+   get '/users', to: 'pages#users'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
