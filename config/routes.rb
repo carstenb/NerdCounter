a1583cf6817bec735424fbe3fba9e9257064dbbe
Carsten Bokemeyer <carsten.bokemeyer@gmx.de>
2013-06-01 16:50:10 +0200

    added users index

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,6 @@ NerdCounter::Application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 
    resources :users
-
-   get '/users', to: 'pages#users'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
