5278d29b4fc0fcf1dc761d648318b407f016ca29
Carsten Bokemeyer <carsten.bokemeyer@gmx.de>
2013-06-01 17:15:47 +0200

    inserted user

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,11 @@ NerdCounter::Application.routes.draw do
 
   root :to => 'pages#index', as: :home_page
 
+  #User resource
+  resources :users
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
-   resources :users
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20120601163500) do
+ActiveRecord::Schema.define(version: 20130601150904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
-    t.string "name"
-    t.string "email"
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
